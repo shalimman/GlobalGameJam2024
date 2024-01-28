8208402902d3e258029093d5a0958e96120097f2
Shawn Halimman <56198376+shalimman@users.noreply.github.com>
2024-01-28 16:10:07 -0500

    wowzers

--- Assets/Scripts/SadPerson.cs
+++ Assets/Scripts/SadPerson.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Laugh
+{
+    Hee,
+    Ha
+};
+
 public class SadPerson : MonoBehaviour
 {
-
+    public int state;
     private Animator anim;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         anim.SetBool("Happy", false);
+        state = 0;
     }
 
     // Update is called once per frame
@@ -19,8 +29,65 @@ public class SadPerson : MonoBehaviour
         
     }
 
+    public void ReceiveInput(Laugh heeha)
+    {
+        switch (state)
+        {
+            case 0:
+                if (heeha == Laugh.Hee)
+                {
+                    anim.SetBool("Alert", true);
+                    state++;
+                }
+                else
+                {
+                    state = 0;
+                    anim.SetBool("Alert", false);
+                }
+                break;
+            case 1:
+                if (heeha == Laugh.Hee)
+                {
+                    state++;
+                }
+                else
+                {
+                    state = 0;
+                    anim.SetBool("Alert", false);
+                }
+                break;
+            case 2:
+                if (heeha == Laugh.Ha)
+                {
+                    state++;
+                }
+                else
+                {
+                    state = 0;
+                    anim.SetBool("Alert", false);
+                }
+                break;
+            case 3:
+                if (heeha == Laugh.Ha)
+                {
+                    state++;
+                    anim.SetBool("Happy", true);
+                }
+                else
+                {
+                    state = 0;
+                    anim.SetBool("Alert", false);
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+            anim.SetBool("Alert", true);
     }
 }
